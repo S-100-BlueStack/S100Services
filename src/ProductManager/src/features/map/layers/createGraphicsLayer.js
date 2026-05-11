da0cf3e2b9ed9d5ceddabe96f66a14ed00868855
Kasper Soelberg Rosenfeldt <karos@gst.dk>
2026-05-11 13:23:45 +0200

    Fixed graphics not showing correctly

--- src/ProductManager/src/features/map/layers/createGraphicsLayer.js
+++ src/ProductManager/src/features/map/layers/createGraphicsLayer.js
@@ -5,19 +5,19 @@ import { geoJsonToGraphics } from "../transformers/geoJsonToGraphics.js";
 import { esriJsonToGraphics } from "../transformers/esriJsonToGraphics.js";
 
 export function createGraphicsLayer(map, layerConfig) {
-  const { id } = layerConfig;
-  let graphics = [];
+  const { data, id, dataFormat = "geojson" } = layerConfig;
 
-  switch (layerConfig.dataFormat) {
-    case "esri-json":
-      graphics = esriJsonToGraphics(layerConfig.data, { layerId: id });
-      break;
-    case "geojson":
-      graphics = geoJsonToGraphics(layerConfig.data, { layerId: id });
-      break;
-    default:
-      throw new Error(`Unsupported data format: ${layerConfig.dataFormat}`);
-  }
+  const graphics = createGraphicsFromData(data, {
+    dataFormat,
+    layerId: id,
+  });
+
+  debugGraphicsLayerCreation({
+    id,
+    data,
+    dataFormat,
+    graphics,
+  });
 
   const index = createLayerIndex(graphics);
 
@@ -28,6 +28,8 @@ export function createGraphicsLayer(map, layerConfig) {
 
   layer.addMany(graphics);
 
+  // Keep custom metadata close to the ArcGIS layer instance so other modules can
+  // treat GraphicsLayer as the runtime representation of an application layer.
   layer.customId = id;
   layer.layerType = "graphics";
   layer._index = index;
@@ -35,4 +37,56 @@ export function createGraphicsLayer(map, layerConfig) {
   map.add(layer);
 
   return layer;
+}
+
+function createGraphicsFromData(data, { dataFormat, layerId }) {
+  switch (dataFormat) {
+    case "geojson":
+      return geoJsonToGraphics(data);
+
+    case "esri-json":
+    case "esrijson":
+      return esriJsonToGraphics(data, { layerId });
+
+    default:
+      throw new Error(`Unsupported graphics data format: ${dataFormat}`);
+  }
+}
+
+function debugGraphicsLayerCreation({ id, data, dataFormat, graphics }) {
+  const graphicsWithoutGeometry = graphics.filter((graphic) => !graphic.geometry);
+
+  console.groupCollapsed(`[Map debug] Graphics layer created: ${id}`);
+
+  console.table({
+    id,
+    dataFormat,
+    graphicsCount: graphics.length,
+    graphicsWithoutGeometry: graphicsWithoutGeometry.length,
+    graphicsWithGeometry: graphics.length - graphicsWithoutGeometry.length,
+  });
+
+  if (graphicsWithoutGeometry.length > 0) {
+    console.warn(
+      `[Map debug] ${graphicsWithoutGeometry.length} graphics in "${id}" have no geometry. They cannot be rendered.`
+    );
+
+    console.log("First raw feature:", getFirstFeature(data));
+    console.log("First graphic without geometry:", graphicsWithoutGeometry[0]);
+    console.log("First graphic attributes:", graphicsWithoutGeometry[0]?.attributes);
+  }
+
+  console.groupEnd();
+}
+
+function getFirstFeature(data) {
+  if (Array.isArray(data)) {
+    return data[0] ?? null;
+  }
+
+  if (Array.isArray(data?.features)) {
+    return data.features[0] ?? null;
+  }
+
+  return null;
 }
